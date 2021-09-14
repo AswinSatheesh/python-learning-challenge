9e58262c5281ab35f9a91e427b10f947619aefde
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2021-09-14 07:45:03 +0530

    Addition program added

---
+++ 14-09-21/basic_programs/add_two_numbers.py
@@ -0,0 +1,20 @@
+#python program to add two numbers
+
+num1 = 15
+num2 = 12
+
+#Adding two numbers and storing it in sum variable!
+sum = num1 + num2 
+
+print("Sum of {0} and {1} is {2}.".format(num1,num2,sum))
+
+# --------------------------------------------------
+#Adding two numbers by accepting input numbers from the user
+
+number1 = float(input("Enter first number :"))
+number2 = float(input("Enter second number :"))
+
+sum = number1 + number2
+
+print("Sum of {0} and {1} is {2}.".format(number1,number2,sum))
+
