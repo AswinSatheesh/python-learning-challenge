9ade5f1a4a713fe16c9345eb82eef73e2f92cf71
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2021-09-15 09:57:13 +0530

    factorial pgm using built in function added

--- 15-09-2021/factorial_of_num.py
+++ 15-09-2021/factorial_of_num.py
@@ -2,11 +2,11 @@
 # factorial of  number using function method
 #Recursive
 
-# def factorial(n):
-#     return 1 if(n == 0 or n == 1) else n * factorial(n-1)
+def factorial(n):
+    return 1 if(n == 0 or n == 1) else n * factorial(n-1)
 
-# num = int(input("Enter a Non-Negative number :"))
-# print("Factiorial of ",num,"is : ",factorial(num))
+num = int(input("Enter a Non-Negative number :"))
+print("Factiorial of ",num,"is : ",factorial(num))
 
 
 # -----------------------------
@@ -31,3 +31,12 @@ print("Factorial of" ,num, "is :",factorial(num))
 
 
 # --------------------------------------
+# By using In-built function:
+
+import math
+
+def factorial(n):
+    return (math.factorial(n))
+
+num = int(input("Enter a Non-Negative number :"))
+print("Factiorial of ",num,"is :",factorial(num))
