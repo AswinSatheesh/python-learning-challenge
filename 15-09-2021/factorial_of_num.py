71d201aef0061d0534684b617a0cd4801eef029b
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2021-09-15 09:44:41 +0530

    factorial prgm using iterative method

--- 15-09-2021/factorial_of_num.py
+++ 15-09-2021/factorial_of_num.py
@@ -1,9 +1,33 @@
 
 # factorial of  number using function method
+#Recursive
+
+# def factorial(n):
+#     return 1 if(n == 0 or n == 1) else n * factorial(n-1)
+
+# num = int(input("Enter a Non-Negative number :"))
+# print("Factiorial of ",num,"is : ",factorial(num))
+
+
+# -----------------------------
+#Iterative method:
+
 
 def factorial(n):
-    return 1 if(n == 0 or n == 1) else n * factorial(n-1)
+    if n < 0:
+        return 0
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        fact = 1
+        while(n>1):
+            fact *= n
+            n -= 1
+        return fact
 
-num = int(input("Enter a Non-Negative number :"))
-print("Factiorial of ",num,"is : ",factorial(num))
+num = int(input("Enter a Non- Negative number :"))
 
+print("Factorial of" ,num, "is :",factorial(num))
+
+
+# --------------------------------------
