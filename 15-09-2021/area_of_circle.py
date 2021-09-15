a8d332812078c54fac9d4fef390f964b554d29ec
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2021-09-15 23:13:53 +0530

    comments added

--- 15-09-2021/area_of_circle.py
+++ 15-09-2021/area_of_circle.py
@@ -5,5 +5,6 @@ def findArea(r):
     return PI * (r * r)
 
 radius = float(input("Enter the radius of circle :"))
-print("Area is %.6f" % findArea(radius))
+
+print("Area is %.6f" % findArea(radius)) #print only the first 6 digits after the point.
 
