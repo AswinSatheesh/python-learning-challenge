84788f3f6058b41bf59d3384199b5c1cc197c61c
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2021-09-17 09:46:31 +0530

    new method added

--- 17-09-2021/list-prgms/interchange_elements.py
+++ 17-09-2021/list-prgms/interchange_elements.py
@@ -1,4 +1,6 @@
 #Python program to swap first and last elements in a list
+# method1
+
 
 # Swap function
 def swap_list(list):
@@ -12,5 +14,16 @@ def swap_list(list):
 list = [12,35,9,56,24]
 print(swap_list(list))
 
-# list1 = [2,5,7,9]
-# print(swap_list(list1))
+list1 = [2,5,7,9]
+print(swap_list(list1))
+
+
+#method2
+
+def swap_list(newlist):
+    newlist[0], newlist[-1] = newlist[-1], newlist[0]
+    return newlist
+
+newlist = [1,2,4,5]
+print(swap_list(newlist))
+
