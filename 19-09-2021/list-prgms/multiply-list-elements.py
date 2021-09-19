866636356b27659b252be6b397b52ffd08df4056
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2021-09-19 12:45:13 +0530

    list multiply prgm

---
+++ 19-09-2021/list-prgms/multiply-list-elements.py
@@ -0,0 +1,11 @@
+
+# Python program to multiply all values in the list using traversal
+ 
+def multiply_list(ls):
+    result = 1   #Initialize the value of the result to 1(not 0 as 0 multiplied with anything returns zero).
+    for value in ls:
+        result = result *value
+    return result 
+
+list = [5, 10, 15, 20, 25]
+print(multiply_list(list))
